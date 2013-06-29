f7587528eee069aef2404000b5330be0e6091cc8
Raphael Estrada <galaktor@gmx.de>
2013-06-29 21:01:58 +0100

    upgrade to Autofac 3.0.2 and include CommandLineTester which was accidentally outside the repo (thanks VS for creating projects in stupid places by default)

---
+++ CommandLineTester/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Configuration;
+
+namespace CommandLineTester
+{
+    public class Foo
+    {
+        private readonly string blah;
+
+        public Foo(string blah)
+        {
+            this.blah = blah;
+        }
+    }
+
+    public class MyModule: CommandLineAwareModule
+    {
+        public string Foo { get; set; }
+        public long Bar { get; set; }
+
+        protected override void _Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Foo>();
+            builder.RegisterAndActivate<ScopeFactory<Foo>>();
+
+            Console.WriteLine("MyModule.Load()");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterModule<MyModule>();
+            var c = cb.Build();
+
+            using (var l = c.BeginLifetimeScope())
+            {
+                var sf = l.Resolve<ScopeFactory<Foo>>();
+                sf.Get(new {blah = "test"});
+            }
+        }
+    }
+}
